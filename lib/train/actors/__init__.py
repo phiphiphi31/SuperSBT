d591bf055ed0e3961a984331e9466b84c90a59ed
phiphi <49226786+phiphiphi31@users.noreply.github.com>
2024-07-10 14:08:02 +0800

    Update __init__.py

--- lib/train/actors/__init__.py
+++ lib/train/actors/__init__.py
@@ -1,5 +1,2 @@
 from .base_actor import BaseActor
-from .ostrack import OSTrackActor
-from .diffusion_actor import DiffusionActor
-from .adasbt import AdaSBTTrackActor
 from .supersbt import SuperSBTActor
