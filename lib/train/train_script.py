b2eb8b693ea8f78b296538d38e87339e3d8c02af
phiphi <49226786+phiphiphi31@users.noreply.github.com>
2024-07-11 10:04:31 +0800

    Update train_script.py

--- lib/train/train_script.py
+++ lib/train/train_script.py
@@ -10,12 +10,10 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 # some more advanced functions
 from .base_functions import *
 # network related
-from lib.models.ostrack import build_ostrack
-from lib.models.adasbt import build_adasbttrack
 from lib.models.supersbt import build_supersbt
 
 # forward propagation related
-from lib.train.actors import OSTrackActor, AdaSBTTrackActor, SuperSBTActor
+from lib.train.actors import SuperSBTActor
 # for import modules
 import importlib
 
@@ -54,11 +52,7 @@ def run(settings):
         cfg.ckpt_dir = settings.save_dir
 
     # Create network
-    if settings.script_name == "ostrack":
-        net = build_ostrack(cfg)
-    elif settings.script_name == "adasbt":
-        net = build_adasbttrack(cfg)
-    elif settings.script_name == "supersbt":
+    if settings.script_name == "supersbt":
         net = build_supersbt(cfg)
     else:
         raise ValueError("illegal script name")
@@ -75,19 +69,7 @@ def run(settings):
     settings.distill = getattr(cfg.TRAIN, "DISTILL", False)
     settings.distill_loss_type = getattr(cfg.TRAIN, "DISTILL_LOSS_TYPE", "KL")
     # Loss functions and Actors
-    if settings.script_name == "ostrack":
-        focal_loss = FocalLoss()
-        objective = {'giou': giou_loss, 'l1': l1_loss, 'focal': focal_loss, 'cls': BCEWithLogitsLoss()}
-        loss_weight = {'giou': cfg.TRAIN.GIOU_WEIGHT, 'l1': cfg.TRAIN.L1_WEIGHT, 'focal': 1., 'cls': 1.0}
-        actor = OSTrackActor(net=net, objective=objective, loss_weight=loss_weight, settings=settings, cfg=cfg)
-
-    elif settings.script_name == "adasbt":
-        focal_loss = FocalLoss()
-        objective = {'giou': giou_loss, 'l1': l1_loss, 'focal': focal_loss, 'cls': BCEWithLogitsLoss()}
-        loss_weight = {'giou': cfg.TRAIN.GIOU_WEIGHT, 'l1': cfg.TRAIN.L1_WEIGHT, 'focal': 1., 'cls': 1.0}
-        actor = AdaSBTTrackActor(net=net, objective=objective, loss_weight=loss_weight, settings=settings, cfg=cfg)
-
-    elif settings.script_name == "supersbt":
+    if  settings.script_name == "supersbt":
         focal_loss = FocalLoss()
         objective = {'giou': giou_loss, 'l1': l1_loss, 'focal': focal_loss, 'cls': BCEWithLogitsLoss()}
         loss_weight = {'giou': cfg.TRAIN.GIOU_WEIGHT, 'l1': cfg.TRAIN.L1_WEIGHT, 'focal': 1., 'cls': 1.0}
